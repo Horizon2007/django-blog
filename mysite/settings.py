3fc27fe38c1e2cb49b72b19098d64cf3d86de9b6
horizonli <horizonli@email.com>
2018-06-22 12:23:25 +0800

    modify STATCIFILES_DIRS to STATICFILES_DIRS

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,8 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+#access static by url
 STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, "collect_static")
-STATCIFILES_DIRS = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'common_static')
     ]
